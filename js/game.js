6655818bda8b8eb9ba50d89284e03d249ebaba4a
Abe Fehr <abe@studocu.com>
2016-06-30 18:02:21 +0200

    Try moving the vibrate one more time

--- js/game.js
+++ js/game.js
@@ -66,7 +66,6 @@ class Cell {
         } else {
             this.element.className = this.element.className.replace(/flagged/,'');
         }
-        window.navigator.vibrate(50);
         this.isFlagged ^= true;
     }
 
@@ -106,6 +105,7 @@ class Cell {
         this.longPressTimer = window.setTimeout(() => {
             if (!this.actionCompleted) {
                 this.toggleFlagged();
+                window.navigator.vibrate(50);
                 this.actionCompleted = true;
             }
         }, 250);
